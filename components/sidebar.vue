c1d07cc2d7ee10c8e5bde313014265bb5f150774
Liu Yajun <herrsechs@foxmail.co>
2017-04-04 12:41:45 +0800

    Initial commit

---
+++ components/sidebar.vue
@@ -0,0 +1,12 @@
+<script>
+    module.exports = {
+        el: "#app",
+        data: function () {
+            return {
+                item1: "",
+                item2: "",
+                item3: ""
+            }
+        }
+    }
+</script>
